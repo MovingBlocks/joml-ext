982c6e4a93eeeda42d7c1ea91cb6db995033f8ca
Michael Pollind <mpollind@gmail.com>
2021-01-16 13:47:32 -0800

    cleanup

--- joml-test/src/main/java/org/terasology/joml/test/VectorAssert.java
+++ joml-test/src/main/java/org/terasology/joml/test/VectorAssert.java
@@ -1,37 +1,13 @@
 package org.terasology.joml.test;
 
-import org.apiguardian.api.API;
 import org.joml.Vector2dc;
 import org.joml.Vector2fc;
 import org.joml.Vector3dc;
 import org.joml.Vector3fc;
-import org.junit.jupiter.api.Assertions;
 import org.junit.platform.commons.util.StringUtils;
 import org.opentest4j.AssertionFailedError;
 
-import static org.apiguardian.api.API.Status.STABLE;
-import static org.junit.jupiter.api.Assertions.assertAll;
-
-@API(status = STABLE, since = "1.0")
 public class VectorAssert {
-
-    /**
-     * Precision for methods that do many operations calculating with a magnitude around zero, giving less accuracy.
-     */
-    public static final float MANY_OPS_AROUND_ZERO_PRECISION_FLOAT = 0.001f;
-    /**
-     * Precision for methods that do basic operations calculating with a magnitude around zero.
-     */
-    public static final float STANDARD_AROUND_ZERO_PRECISION_FLOAT = 0.000000000000000001f;
-
-    /**
-     * Precision for methods that do many operations calculating with values with a magnitude around zero, giving less accuracy.
-     */
-    public static final double MANY_OPS_AROUND_ZERO_PRECISION_DOUBLE = 0.00001;
-    /**
-     * Precision for methods that do basic operations calculating with a magnitude around zero.
-     */
-    public static final double STANDARD_AROUND_ZERO_PRECISION_DOUBLE = 0.000000000000000000001;
 
     public static void assertEquals(Vector3fc expected, Vector3fc actual, float epsilon) {
         if (expected == null) {
