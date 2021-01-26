93642c353884ee8fbf8bb36fcc68299af1127bde
Tobias Nett <skaldarnar@googlemail.com>
2021-01-26 20:40:11 +0100

    test: add test for AABBd; rename test method

--- joml-geometry/src/test/java/org/terasology/joml/geom/AABBdTest.java
+++ joml-geometry/src/test/java/org/terasology/joml/geom/AABBdTest.java
@@ -2,15 +2,15 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.joml.geom;
 
-import org.joml.Vector3f;
+import org.joml.Vector3d;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class AABBfTest {
+public class AABBdTest {
     @Test
     public void testZeroSizeContains() {
-        assertFalse(new AABBf(new Vector3f(0, 0, 0)).containsPoint(0, 0, 0));
-        assertFalse(new AABBf(new Vector3f(.5f, .5f, .5f)).containsPoint(.5f, .5f, .5f));
+        assertFalse(new AABBd(new Vector3d(0, 0, 0)).containsPoint(0, 0, 0));
+        assertFalse(new AABBd(new Vector3d(.5, .5, .5)).containsPoint(.5, .5, .5));
     }
 }
